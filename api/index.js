eb2fba82e687e0171b6a70900d2f9dbf0e5d1bd0
codehub19 <luckybhuskute@gmail.com>
2024-06-09 16:42:26 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 dotenv.config();
 
@@ -17,9 +18,11 @@ const app = express();
 const port = 3000;
 
 
-app.get('/' , (req,res) => {
-    res.send('hello my blog app')
-})
 app.listen(port , () => {
     console.log(`server is listening on port ${port}!`)
 });
+
+
+app.use('/api/user', userRoutes);
+
+// app.get('/' ,)
